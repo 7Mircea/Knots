c2fbaad061384fd42f4db07e736eff673f299ffd
Gheoace Mircea <7mircea.c@gmail.com>
2021-01-06 13:18:51 +0200

    start working on Drawer

--- app/src/main/java/com/example/myfundamentalsapplication/MainActivity.java
+++ app/src/main/java/com/example/myfundamentalsapplication/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.myfundamentalsapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String SHARED_PREFS = "prefs";
     private static final String SHARED_PREFS_REGISTERED = "alreadyRegistered";
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //stabilim daca utilizatorul este logat sau nu
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
 
         Intent incomingIntent = getIntent();
@@ -30,5 +43,40 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,RegistrationActivity.class);
             startActivity(intent);
         }
+
+        //ne ocupam de interfata
+        //punem toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //setam navigation view
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer
+                ,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container,new MainFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_knots);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_knots :
+                Toast.makeText(this,"Ai apasat pe knots",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_questions :
+                Toast.makeText(this,"Ai apasat pe questions",Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
